21c975c7e695754c68bc53732551ac2392dcfcf5
eduardo_hgombrade <eduardo.hgombrade2005@gmail.com>
2026-08-23 11:49:56 -0300

    Update simulation results page and add simulation card component

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -24,8 +24,8 @@ export const SimulationForm = () => {
     console.log({ updatedFormData })
 
     if (currentStepIndex + 1 > totalSteps - 1) {
-      saveFormData(updatedFormData)
-      void navigate('/resultado')
+      const id = saveFormData(updatedFormData)
+      void navigate(`/resultado/${id}`)
       return
     }
 
